4de9b9a64d258e8b770c46eb8f12715848b2caa1
s202255656 <s202255656@pusan.ac.kr>
2025-12-31 19:59:41 +0900

    [ ChunkGeneratorManager 구현 ]

---
+++ Source/ColorDefense/ChunkGeneratorManager.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChunkGeneratorManager.h"
+
+void UChunkGeneratorManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+    this->ChunkGenerator = nullptr;
+}
+
+void UChunkGeneratorManager::Deinitialize()
+{
+    this->ClearChunkGenerator();
+    Super::Deinitialize();
+}
+
+void UChunkGeneratorManager::CreateChunkGenerator(UChunkGrid* InChunkGrid)
+{
+    // 1. 기존 생성기가 있다면 정리 (GC가 나중에 수거해가도록 참조 해제)
+    ClearChunkGenerator();
+
+    // 2. 새 로직 객체 생성 (NewObject)
+    // Outer를 'this'(Subsystem)로 설정하여 Subsystem이 소유권을 가짐
+    UChunkGenerator* NewChunkGenerator = NewObject<UChunkGenerator>(this, UChunkGenerator::StaticClass());
+
+    // 3. 데이터 주입 및 초기화
+    NewChunkGenerator->Initialize(InChunkGrid);
+    this->ChunkGenerator = NewChunkGenerator; // 멤버 변수에 저장하여 GC 방지
+}
+
+void UChunkGeneratorManager::ClearChunkGenerator()
+{
+    if (this->ChunkGenerator)
+    {
+        // 필요한 경우 여기서 Generator의 정리 함수 호출 (예: 데이터 저장 등)
+        this->ChunkGenerator = nullptr; // 참조를 끊으면 GC가 다음 사이클에 수거함
+    }
+}
---
+++ Source/ColorDefense/ChunkGeneratorManager.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ChunkGenerator.h" // ChunkGenerator를 관리하기 위해
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "ChunkGeneratorManager.generated.h"
+
+UCLASS()
+class COLORDEFENSE_API UChunkGeneratorManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	void CreateChunkGenerator(UChunkGrid* InChunkGrid);
+	void ClearChunkGenerator();
+public:
+	UPROPERTY()
+	UChunkGenerator* ChunkGenerator;
+};
